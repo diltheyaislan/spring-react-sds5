5d6cfc7a00d349294ba5922edc11d3b460838ede
Dilthey Aislan <diltheyaislan@gmail.com>
2021-11-01 19:25:42 -0300

    add static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,33 @@
 import NavBar from 'components/NavBar';
 import Footer from 'components/Footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
 
 function App() {
   return (
 	<>
 		<NavBar />
 		<div className='container'>
-			<h1 className='text-primary'>DSVendas APP</h1>
+			<h1 className='text-primary py-3'>Dashboard de vendas</h1>
+
+			<div className='row px-3'>
+				<div className='col-sm-6'>
+					<h5 className='text-center text-secondary'>Success rate (%)</h5>
+					<BarChart />
+				</div>
+				<div className='col-sm-6'>
+					<h5 className='text-center text-secondary'>All sales</h5>
+					<DonutChart />
+				</div>
+			</div>
+
+			<div className='py-3'>
+				<h2 className='text-primary'>All sales</h2>
+			</div>
+
+			<DataTable />
 		</div>
-		<DataTable />
 		<Footer />
 	</>
   );
