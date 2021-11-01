2d8f4971a9badc40b8e1b8e089387d95da73aefd
Dilthey Aislan <diltheyaislan@gmail.com>
2021-11-01 19:09:31 -0300

    add layout base

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<footer className="footer mt-auto py-3 bg-dark">
+			<div className="container">
+				<p className="text-light">App developed by <a href="https://github.com/diltheyaislan" target="_blank" rel="noreferrer">Dilthey Aislan</a></p>
+				<p className="text-light"><small><strong>Semana Spring React</strong><br/>
+				Event offered by DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
